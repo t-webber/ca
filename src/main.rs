c3536a80ac98c7d5c7cfa4fe971c4fbaa8f24b55
Tom Webber <t.c.w.webber@gmail.com>
2026-03-29 20:40:56 +0200

    chore(cargo): add clippy rules

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
-}
+//! Caw, a better cargo add.
+#![allow(
+    clippy::single_call_fn,
+    clippy::implicit_return,
+    clippy::pattern_type_mismatch,
+    clippy::blanket_clippy_restriction_lints,
+    clippy::missing_trait_methods,
+    clippy::question_mark_used,
+    clippy::mod_module_files,
+    clippy::module_name_repetitions,
+    clippy::pub_with_shorthand,
+    clippy::unseparated_literal_suffix,
+    clippy::else_if_without_else,
+    reason = "bad lints"
+)]
+
+const fn main() {}
